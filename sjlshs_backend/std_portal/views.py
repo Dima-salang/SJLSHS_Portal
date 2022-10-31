598c65a576117dc623364002032b22ef8eeaa75a
Midnight <luisgabrielle1026@gmail.com>
2022-10-31 13:56:57 +0800

    hello

--- sjlshs_backend/std_portal/views.py
+++ sjlshs_backend/std_portal/views.py
@@ -7,3 +7,7 @@ from django.http import HttpResponse
 
 def home_page_view(request):
     return render(request, "home.html", {})
+
+
+def about_page_view(request):
+    return render(request, "portal-home.html", {})
